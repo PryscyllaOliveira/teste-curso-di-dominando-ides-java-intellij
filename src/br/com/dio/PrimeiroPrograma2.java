4c5111a51f12d7d70f4b21fb8d451dee9c19de18
PryscyllaOliveira <pryscyllaestetica@gmail.com>
2022-07-15 19:06:43 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma2.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import jdk.swing.interop.SwingInterOpUtils;
+import model.Gato;
+
+import java.sql.SQLOutput;
+
+public class PrimeiroPrograma2 {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello Word!!!" + (a+b));*/
+    }
+}
